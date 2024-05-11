4fdef5451ca363034b67cf6ed874a145f60a003f
victor alvarez <victorfelipealvarezrojas@hotmail.com>
2024-05-11 16:20:58 -0400

    agrego autorizacion basada en permisos

--- src/main/java/com/cursos/api/springsecuritycourse/config/security/HttpSecurityConfig.java
+++ src/main/java/com/cursos/api/springsecuritycourse/config/security/HttpSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.cursos.api.springsecuritycourse.config.security;
 
 import com.cursos.api.springsecuritycourse.config.security.filter.JwtAuthenticationFilter;
+import com.cursos.api.springsecuritycourse.persistence.util.RolePermission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,32 +9,66 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity // arranca la creacion de componentes de s.security (AuthConfiguration, authEntryPoint(respuesta))
+@EnableWebSecurity
 public class HttpSecurityConfig {
 
     @Autowired
     private AuthenticationProvider daoAuthenticationProvider;
 
     @Autowired
-    private JwtAuthenticationFilter jwtAuthenticationFilter; // filtro personalizado
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf(csrf -> csrf.disable())
-                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(daoAuthenticationProvider) //cuando realice un authenticate usara esta estrategia desde login
-                // sett al securityContextHolder, es necesario para que AuthenticationService que se alimenta del holder
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(daoAuthenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .authorizeHttpRequests(authReqConfig -> {
-                    authReqConfig.requestMatchers(HttpMethod.POST,"/customers").permitAll();
-                    authReqConfig.requestMatchers(HttpMethod.POST,"/auth/authenticate").permitAll();
-                    authReqConfig.requestMatchers(HttpMethod.GET,"/auth/validate").permitAll();
-                    authReqConfig.anyRequest().authenticated();
-                }).build();
+                .authorizeHttpRequests(HttpSecurityConfig::buildRequestMatches).build();
+    }
+
+    private static void buildRequestMatches(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authReqConfig) {
+        /*
+         * Private Authorization endpoint products
+         */
+        authReqConfig.requestMatchers(HttpMethod.GET, "/products")
+                .hasAuthority(RolePermission.READ_ALL_PRODUCTS.name());
+        authReqConfig.requestMatchers(HttpMethod.GET, "/products/{productId}")
+                .hasAuthority(RolePermission.READ_ONE_PRODUCTS.name());
+        authReqConfig.requestMatchers(HttpMethod.POST, "/products")
+                .hasAuthority(RolePermission.CREATE_ONE_PRODUCTS.name());
+        authReqConfig.requestMatchers(HttpMethod.PUT, "/products/{productId}")
+                .hasAuthority(RolePermission.UPDATE_ONE_PRODUCTS.name());
+        authReqConfig.requestMatchers(HttpMethod.PUT, "/products/{productId}/disabled")
+                .hasAuthority(RolePermission.DISABLE_ONE_PRODUCTS.name());
+        /*
+         * Private Authorization endpoint categories
+         */
+        authReqConfig.requestMatchers(HttpMethod.GET, "/categories")
+                .hasAuthority(RolePermission.READ_ALL_PRODUCTS.name());
+        authReqConfig.requestMatchers(HttpMethod.GET, "/categories/{categoryId}")
+                .hasAuthority(RolePermission.READ_ONE_PRODUCTS.name());
+        authReqConfig.requestMatchers(HttpMethod.POST, "/categories")
+                .hasAuthority(RolePermission.CREATE_ONE_PRODUCTS.name());
+        authReqConfig.requestMatchers(HttpMethod.PUT, "/categories/{categoryId}")
+                .hasAuthority(RolePermission.UPDATE_ONE_PRODUCTS.name());
+        authReqConfig.requestMatchers(HttpMethod.PUT, "/categories/{categoryId}/disabled")
+                .hasAuthority(RolePermission.DISABLE_ONE_PRODUCTS.name());
+        /*
+         * Public endpoint
+         */
+        authReqConfig.requestMatchers(HttpMethod.POST, "/customers").permitAll();
+        authReqConfig.requestMatchers(HttpMethod.POST, "/auth/authenticate").permitAll();
+        authReqConfig.requestMatchers(HttpMethod.GET, "/auth/validate").permitAll();
+
+        // Authentication
+        authReqConfig.anyRequest().authenticated();
     }
 }
